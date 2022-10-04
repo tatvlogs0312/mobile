dac288ee10e5cda8c29e14a3deda1c061f60efd5
tatuan0312 <85239207+tatvlogs0312@users.noreply.github.com>
2022-10-04 09:47:47 +0700

    update api product

--- BT08/server.js
+++ BT08/server.js
@@ -1,6 +1,18 @@
-let express = require("express");
-let app = express();
-let port = process.env.PORT || 3000;
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+require("dotenv").config();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+let routes = require("./api/routes"); //importing route
+routes(app);
+
+app.use(function (req, res) {
+  res.status(404).send({ url: req.originalUrl + " not found" });
+});
 
 app.listen(port);
 
